9d11b50b9f3ed4e36112b63fbf7018137c2cd95f
luigipr <luigiptofeti@gmail.com>
2024-09-09 15:43:26 -0300

    feature: innitial setup and development

---
+++ src/recintos-zoo.js
@@ -0,0 +1,12 @@
+class RecintosZoo {
+
+    analisaRecintos(animal, quantidade) {
+
+        ZooService
+
+        return 
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
---
+++ src/services/index.js
@@ -0,0 +1,14 @@
+
+export async function(animal, quantidade) {
+
+    animais = repositories
+
+    if(!animais.contains(animal)) {
+        return {
+            erro: "Animal inválido"
+          }
+    }
+
+    
+
+}
